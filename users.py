9218e57999f6ecdcb0eab90c964d11592b7cebe8
Danylo <danykyurkevych@gmail.com>
2024-03-09 13:14:31 +0100

    Mastered structure

--- users.py
+++ users.py
@@ -1,40 +1,38 @@
 import re
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from permissions import UserPermission, ModeratorPermission, AdminPermission
 
 
 class AbstractUser(ABC):
-    pass
-
-
-class User(AbstractUser):
     def __init__(self, name: str, surname: str, email: str):
         self.name = name
         self.surname = surname
-        self.permissions = {'message_write': True,
-                            'message_change': True,
-                            'message_delete': False,
-                            'message_export': False}
+        self.email = AbstractUser._validate_email(email)
+        self.permission = self._set_permission()
 
-        self.email = self._validate_email(email)
-
-    def _validate_email(self, email):
+    @staticmethod
+    def _validate_email(email) -> str:
         if re.match(r'[^@]+@[^@]+\.[^@]+', email):
             return email
-
         else:
             raise ValueError(f'Invalid email address: {email}')
 
-
-class Moderator(User):
-    def __init__(self, name: str, surname: str, permissions: dict):
-        super().__init__(name, surname, permissions)
-
-        self.permissions['message_delete'] = True
+    @abstractmethod
+    def _set_permission(self):
+        pass
 
 
-class Admin(Moderator):
-    def __init__(self, name: str, surname: str, permissions: dict):
-        super().__init__(name, surname, permissions)
+class User(AbstractUser):
+    def _set_permission(self):
+        return UserPermission()
 
-        self.permissions['message_export'] = True
 
+class Moderator(AbstractUser):
+    def _set_permission(self):
+        return ModeratorPermission()
+
+
+class Admin(AbstractUser):
+    def _set_permission(self):
+        return AdminPermission()
