9d209cc5551f81877aa07369a8973e7d7e2af929
Danylo <danykyurkevych@gmail.com>
2024-03-11 11:44:55 +0100

    Initial commit

---
+++ conn_settings.py
@@ -0,0 +1,11 @@
+# Description: Connection settings for the databases
+
+POSTGRE_PARAMS = {'host': 'host',
+                  'database': 'db_name_postgre',
+                  'user': 'user',
+                  'port': 5432}
+
+MYSQL_PARAMS = {'host': 'host',
+                'database': 'db_name_mysql',
+                'user': 'user',
+                'port': 3306}
